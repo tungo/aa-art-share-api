ce11ccb75477af6e3e0c0e6bb59a1860a3f55cb1
Tu Ngo <tungo8810@gmail.com>
2017-04-17 12:13:27 -0700

    routes complete

--- Gemfile
+++ Gemfile
@@ -45,6 +45,17 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+
+  gem 'annotate'
+
+# These two give you a great error handling page.
+# But make sure to never use them in production!
+  gem 'better_errors'
+  gem 'binding_of_caller'
+
+
+  # pry > irb
+  gem 'pry-rails'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # resources :users, only: [:index, :show, :create, :update, :destroy, :new]
+
+  get 'users' => 'users#index'
+  post 'users' => 'users#create'
+  get 'users/new' => 'users#new', :as => 'new_user'
+  get 'users/:id' => 'users#show', :as => 'user'
+  patch 'users/:id' => 'users#update'
+  put 'users/:id' => 'users#update'
+  delete 'users/:id' => 'users#destroy'
+
+
+
 end
