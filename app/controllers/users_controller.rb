3b5ed5b8c6ae1f4d640780088567eb867b157757
Tu Ngo <tungo8810@gmail.com>
2017-04-17 14:30:00 -0700

    complete first routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,45 @@
 class UsersController < ApplicationController
 
   def index
-    render json: params
+    render json: User.all
   end
 
   def create
-    render json: params
+    user = User.new(user_params)
+    if user.save
+      render json: user
+    else
+      render(
+        json: user.errors.full_messages, status: :unprocessable_entity
+      )
+    end
   end
 
   def show
-    render json: params
+    begin
+      user = User.find(params[:id])
+      render json: user
+    rescue
+      render text: 'User id is not exists!!!'
+    end
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    render json: user
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.delete
+    render json: user
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+
   end
 end
