3e27c26da36653d2014e2d3afa8aae3f14312fec
Tu Ngo <tungo8810@gmail.com>
2017-04-17 15:29:05 -0700

    Add models and seed

---
+++ app/models/artwork.rb
@@ -0,0 +1,18 @@
+class Artwork < ApplicationRecord
+  validates :title, :image_url, :artist_id, presence: true
+  validates :title, uniqueness: { scope: :artist_id }
+
+  belongs_to :artist,
+    primary_key: :id,
+    foreign_key: :artist_id,
+    class_name: "User"
+
+  has_many :artwork_shares,
+    primary_key: :id,
+    foreign_key: :artwork_id,
+    class_name: "ArtworkShare"
+
+  has_many :shared_viewers,
+    through: :artwork_shares,
+    source: :viewer
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.delete_all
+u1 = User.create(username: 'Monte')
+u2 = User.create(username: 'Tu')
+u3 = User.create(username: 'Picasso')
+u4 = User.create(username: 'Monet')
+
+Artwork.delete_all
+a1 = Artwork.create(title: 'The Weeping Woman', image_url: 'some_url.aa', artist_id: u3.id)
+a2 = Artwork.create(title: 'The Old Guitarist', image_url: 'some_other_url.aa', artist_id: u3.id)
+a3 = Artwork.create(title: 'Poppies', image_url: 'some_url.aa', artist_id: u4.id)
+a4 = Artwork.create(title: 'Water Lily Pond', image_url: 'some_other_url.aa', artist_id: u4.id)
+
+ArtworkShare.delete_all
+ArtworkShare.create(artwork_id: a1.id, viewer_id: u1.id)
+ArtworkShare.create(artwork_id: a2.id, viewer_id: u1.id)
+ArtworkShare.create(artwork_id: a3.id, viewer_id: u2.id)
+ArtworkShare.create(artwork_id: a4.id, viewer_id: u2.id)
+ArtworkShare.create(artwork_id: a1.id, viewer_id: u2.id)
+ArtworkShare.create(artwork_id: a2.id, viewer_id: u2.id)
